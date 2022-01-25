a94b6b51772dc7baae7dca224a4be88ace8db2e5
Justin Scott Bieshaar <87446388+JustinScottBieshaar@users.noreply.github.com>
2022-01-25 21:41:59 +0100

    Update Util.cs

--- Assets/Code/Util.cs
+++ Assets/Code/Util.cs
@@ -1,17 +1,3 @@
-/*
- * Copyright (c) 2021 Gamepoint. All Rights Reserved.
- * 
- * Woe to the traitor who tries to penetrate my secrets and to stretch forth a sacrilegious
- * hand toward my code. May all calamities and disgrace fall on him! May hunger twist his
- * entrails, and sleep flee from his bloodshot eyes! May the hand of the man wither who
- * hastens to him with rescue and pities him in his misery! May the bread on his table turn
- * into rottenness, and the wine into stinking juice! May his children die out, and his house
- * be filled with bastards who will spit on him and expel him! May he die groaning through many
- * days in loneliness, and may neither earth nor water receive his vile carcass, may no fire burn
- * it, no wild beasts devour it! And may his soul, torn by its sins, wander without rest,
- * through dark places.
- */
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
